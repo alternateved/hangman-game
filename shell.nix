5ff1a7bc3c8ef70d2073097b073a46c85b149e73
alternateved <alternateved@gmail.com>
2021-10-16 19:40:26 +0200

    Add nix files

---
+++ shell.nix
@@ -0,0 +1,22 @@
+let
+  pkgs = import ./packages.nix { };
+  package = import ./.;
+
+  haskellTools = with pkgs; [
+    ghc
+    ghcid
+    cabal-install
+    haskellPackages.hindent
+    haskellPackages.hoogle
+    haskellPackages.ormolu
+    haskellPackages.haskell-language-server
+  ];
+
+in pkgs.mkShell {
+  nativeBuildInputs = [ pkgs.pkg-config ];
+  inputsFrom = [ package.env ];
+  buildInputs = haskellTools;
+  shellHook = ''
+    export PS1="\[hs:\033[1;32m\]\W\[\033[0m\] ~ "
+  '';
+}
