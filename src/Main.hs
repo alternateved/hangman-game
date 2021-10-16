31f2c7c25060247fbbff3d7993214c7e1bd82d17
alternateved <alternateved@gmail.com>
2021-10-16 19:48:04 +0200

    Import modules

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,11 @@
 module Main where
 
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.List (intersperse)
+import Data.Maybe (isJust)
+import System.Exit (exitSuccess)
+import System.Random (randomRIO)
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
