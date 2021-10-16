6a8ae80de7e7d92789035ebe111340a31be1abf4
alternateved <alternateved@gmail.com>
2021-10-16 23:03:26 +0200

    Add functions to pull a random word from the gameWords list

--- src/Main.hs
+++ src/Main.hs
@@ -9,16 +9,36 @@ import System.Random (randomRIO)
 
 type WordList = [String]
 
+-- Generate a list of words from file
 allWords :: IO WordList
 allWords = do
   dict <- readFile "data/dict.txt"
   return (lines dict)
 
+-- Set words length limit
 minWordLength :: Int
 minWordLength = 5
 
 maxWordLength :: Int
 maxWordLength = 9
+
+-- Filter allWords accordingly to provided word's length constraint
+gameWords :: IO WordList
+gameWords = do filter gameLength <$> allWords
+  where
+    gameLength w =
+      let l = length (w :: String)
+       in l >= minWordLength && l < maxWordLength
+
+-- Generate random number and return a word from WordList with index of random number
+randomWord :: WordList -> IO String
+randomWord wl = do
+  randomIndex <- randomRIO (0, length wl - 1)
+  return $ wl !! randomIndex
+
+-- Get random word from gameWords by binding gameWords to randomWord
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
